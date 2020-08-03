0e33d78d0d221e2a13e25e23a237aa60e34c3ad0
Duck-duck-duan <decadeduan@163.com>
2020-08-03 20:44:31 +0800

    modified repetitions.py

--- CSES/repetitions.py
+++ CSES/repetitions.py
@@ -1,18 +1,14 @@
 def repetitions(dnaStr):
-    len_rep, num = 0, 0
-    x = dnaStr[0]
-    for i in range(len(dnaStr)):
-        if i != 0 and x == dnaStr[i]:
-            #print('enter')
+    len_rep, num = 1, 1
+    for i in range(0, len(dnaStr) - 1):
+        if dnaStr[i] == dnaStr[i + 1]:
             num += 1
-            if i == len(dnaStr) - 1 and len_rep < num: len_rep = num 
+            len_rep = max(len_rep, num)
         else: 
-            x = dnaStr[i]
-            if len_rep < num: 
-                len_rep = num 
-                num = 0
+            len_rep = max(len_rep, num)
+            num = 1
         
-    return len_rep + 1
+    return len_rep
 
 dnaStr = input()
 print(repetitions(dnaStr))
